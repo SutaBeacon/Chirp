06639657505b56497ed9ff4c37521ed538489bd1
Sean Zhang <seanzhang@tongji.edu.cn>
2018-06-04 14:07:47 +0800

    示例与文档

--- src/Base/SafeProcess.py
+++ src/Base/SafeProcess.py
@@ -95,22 +95,30 @@ if __name__ == '__main__':
 
     class TestProcess (SafeProcess):
 
-        def test(self, t):
+        def testAlarm(self, t):
             print("timer at", t)
 
         def setup(self):
+            # 会在进程开始时运行一次
             print("setup")
+            # 设一个一秒钟之后触发的 alarm，触发时调用 testAlarm
+            self.setAlarm(1.0, testAlarm)
 
         def loop(self):
+            # 会在进程运行过程中不断重复被调用
             print("loop-delay1")
+            # delay 0.2 秒
             self.delay(0.2)
             print("loop-delay2")
-            self.delay(0.2)
+            # delay 0.4 秒
+            self.delay(0.4)
 
         def onMessage(self, msg):
+            # 当某进程给本进程发送消息时，onMessage 会自动被调用
             print("onMessage:", msg)
 
         def onExit(self):
+            # 当本进程即将结束时，onExit 会自动被调用
             print("onExit")
 
     p = TestProcess()
