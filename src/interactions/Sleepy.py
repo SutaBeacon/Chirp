2b339a2e6bcd447c89e149b6a42a37d4c36f72d8
XUMing <xuming0216@gmail.com>
2018-06-21 03:17:13 +0800

    Interaction-sleepy
    sleepy添加了一下，看下有啥问题么。。

--- src/interactions/Sleepy.py
+++ src/interactions/Sleepy.py
@@ -1,27 +1,22 @@
 from Interaction import Interaction
-from MusicAnalyzer import predict
-from data.phrases import angry
-from data.phrases import question
+from ConsoleLog import notice
+from data.phrases import snore
 
 
 class Sleepy(Interaction):
 
     def setup(self):
         self.registerHandler('midi', self.onMidi)
-        self.makeFace("Happy.json")
+        self.setInstrument(73)
+        self.makeFace('Sleepy.json')
+
+    def loop(self):
+        self.sing(snore)
 
     def onMidi(self, msg):
-        if msg['cmd'] == 'phrase-end':
-            p = predict(msg['notes'])
-            if p[0][0] == 'Wake Up' and p[1] > 0.6:
-                self.sing(angry, self.finish)
-                self.makeFace(2)
-            elif p[0][0] == "Call" and p[1] > 0.8:
-                self.sing(question, self.finish)
-                self.makeFace(0)
-            elif p[0][0] == "Play" and p[1] > 0.8:
-                self.sing(question, self.finish)
-                self.makeFace(1)
+        notice("Received phrase: {} at {}-{}".format(msg['notes'], msg['start'], msg['end']))
+        self.sing(wakeup, self.finish)
+        self.makeFace('Wakeup.json')
 
     def finish(self, t):
         self.terminate()
